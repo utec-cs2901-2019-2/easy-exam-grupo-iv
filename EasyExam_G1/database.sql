4051a938c7c67aad6610ba0d6b5a29a66a2ea5b7
Nadnus <fernandodelosheros@gmail.com>
2019-10-30 12:42:01 -0500

    Normed db

--- EasyExam_G1/database.sql
+++ EasyExam_G1/database.sql
@@ -1,10 +1,38 @@
 
-create table Exam();
-create table Questions();
-create table User
+create table Exam
 (
-    bool isMod,
+    college varchar(30),
+    course varchar(30),
+    creation_date date,
+    max_points int,
+    question_number int,
+    rules text,
+    specs text,
+    title varchar(30),
+
 );
-create table questionsPerUSer();
-create table reportsPerAdmin();
-create table 
+create table Questions
+(
+    question_id serial,
+    is_disabled bool,
+    creation_date date,
+    question_description text,
+    title varchar(100),
+    score int,
+    pkey question_id,
+
+);
+create table Users
+(
+    is_mod bool,
+    email VARCHAR(50),
+    points int,
+
+);
+create table QuestionsPerUSer();
+CREATE table QuestionsPerExam();
+create table ReportsPerAdmin();
+create table SubcategoriesPerQuestion();
+create table categories();
+create table Subcategories();
+create table ExamsPerUser();
