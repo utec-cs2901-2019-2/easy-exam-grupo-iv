5065bde9a197d9a1c81fcae12b179ef5c111eaff
Andre <andre.segovia@utec.edu.pe>
2019-11-07 14:57:30 -0500

    Finished Login Frontend

--- EasyExam/Front/EasyExam/src/app/shared/services/auth.service.ts
+++ EasyExam/Front/EasyExam/src/app/shared/services/auth.service.ts
@@ -1,15 +1,46 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from './http.service';
+import { User } from '../interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  emailChecker = `^[a-zA-Z0-9.!#$%&'*+/=?^_\`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-z
+A-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$`;
   admin: boolean;
-  user: any;
+  user: User;
   constructor(private http: HttpService) { }
-  login(email: string, password: string): boolean {return true; }
-  register(name: string, email: string, password: string): boolean {return true; }
-  logout() {}
+  login(email: string, password: string): boolean {
+    this.user = this.http.login(email, password);
+    if (this.user == null) {
+      return false;
+    }
+    return true;
+  }
+  register(Name: string, Email: string, Password: string): boolean {
+    this.http.register(Name, Email, Password)
+    this.user = {
+      email : Email,
+      lastName : '',
+      name : Name,
+      password : Password,
+      phone : ''
+    };
+    return true;
+  }
+  logout() {
+    localStorage.removeItem('access_token');
+    this.user = {
+      email : '',
+      lastName : '',
+      name : '',
+      password : '',
+      phone : ''
+    };
+  }
+  loggedIn(): boolean {
+    return localStorage.getItem('access_token') !== null;
+  }
   forgotPassword(email: string) {}
 }
--- EasyExam/Front/EasyExam/src/app/shared/services/http.service.ts
+++ EasyExam/Front/EasyExam/src/app/shared/services/http.service.ts
@@ -1,27 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
 import { User } from '../interfaces/user';
 import { Exam } from '../interfaces/exam';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class HttpService {
-  url: string;
+  url = '';
+  loginurl = '';
+  registerurl = '';
+  forgoturl = '';
   constructor(private http: HttpClient) { }
-  login(email: string, password: string): User {
-    let user: User = {
-      email: '',
-      lastName: '',
-      name: '',
-      password: '',
-      phone: ''
-    };
+  login(Email: string, Password: string): User {
+    let user: User;
+    this.http.post<{user: User; token: string}>(this.url + this.loginurl, {email: Email, password: Password})
+    .subscribe(
+      (data) => {
+        user = data.user;
+        localStorage.setItem('access_token', data.token);
+      },
+      (err) => {
+        user = null;
+        console.log(err);
+      }
+    );
     return user;
   }
-  register(name: string, email: string, password: string) {}
-  logout() {}
-  forgotPassword(email: string) {}
+  register(Name: string, Email: string, Password: string) {
+    this.http.post<{token: string}>(this.url + this.registerurl, {name: Name, email: Email, password: Password})
+    .subscribe(
+      (data) => {
+        localStorage.setItem('access_token', data.token);
+      }
+    );
+  }
+  forgotPassword(Email: string) {
+    this.http.post(this.url + this.registerurl, { email: Email });
+  }
   getlang(): any {}
   changelang(value: string): any {}
   getpdf(texstring: string, savevalue: Exam) {}
