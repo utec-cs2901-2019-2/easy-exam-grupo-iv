d33418e6cc7a7ca7f24370d743dfed030beb5814
Nadnus <fernando.delosheros@utec.edu.pe>
2019-11-07 13:48:58 -0500

    acabe tablas, faltan repos

--- g4/src/main/java/model/Subcategory.java
+++ g4/src/main/java/model/Subcategory.java
@@ -6,4 +6,23 @@ import javax.persistence.Entity;
 public class Subcategory {
     String name;
     Category category;
+
+    public Subcategory() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
--- g4/src/main/java/model/TeacherRepository.java
+++ g4/src/main/java/model/TeacherRepository.java
@@ -1,4 +1,8 @@
 package model;
 
+import javax.persistence.Entity;
+
+@Entity
 public interface TeacherRepository {
+
 }
