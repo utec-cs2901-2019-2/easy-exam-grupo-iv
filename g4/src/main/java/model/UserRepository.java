ae90c3fd64a6ac405d3c7a4f7d67678b6b186a90
Nadnus <fernando.delosheros@utec.edu.pe>
2019-11-06 21:45:23 -0500

    first table in java

---
+++ g4/src/main/java/model/UserRepository.java
@@ -0,0 +1,11 @@
+package model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,String >{
+    List<User> findAllByEmail(Iterable<String> strings);
+}
